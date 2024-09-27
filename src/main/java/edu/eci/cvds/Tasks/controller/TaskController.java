f8960956a61a694d024f67f26c41687843dd9f45
Juan Sebastian Velasquez <sebastianvelasquez2807@gmail.com>
2024-09-26 20:56:29 -0500

    implemetacion del taskController

--- src/main/java/edu/eci/cvds/Tasks/controller/TaskController.java
+++ src/main/java/edu/eci/cvds/Tasks/controller/TaskController.java
@@ -7,6 +7,35 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
 
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable String id) {
+        return taskService.getTaskById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable String id, @RequestBody Task updatedTask) {
+        return taskService.updateTask(id, updatedTask);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable String id) {
+        taskService.deleteTask(id);
+    }
 }
