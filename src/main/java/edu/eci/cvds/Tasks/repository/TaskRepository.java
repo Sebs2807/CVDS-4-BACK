b678cbdb4273e52b106ba6694ed577ad46346ad1
santi-123 <santiago.diaz-r@mail.esculaing.edu.co>
2024-09-28 07:25:00 -0500

    Implementacion del TaskRepository

--- src/main/java/edu/eci/cvds/Tasks/repository/TaskRepository.java
+++ src/main/java/edu/eci/cvds/Tasks/repository/TaskRepository.java
@@ -4,5 +4,4 @@ import edu.eci.cvds.Tasks.model.Task;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface TaskRepository extends MongoRepository<Task, String> {
-    
 }
