dfd43dc24b8e686c1bd20333a4ffb9753ddb2103
Juan Sebastian Velasquez <sebastianvelasquez2807@gmail.com>
2024-10-08 19:38:02 -0500

    hotfix santiago

--- src/main/java/edu/eci/cvds/Tasks/configuration/WebConfig.java
+++ src/main/java/edu/eci/cvds/Tasks/configuration/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:8081", "http://127.0.0.1:5500")
+                .allowedOrigins("http://127.0.0.1:8081", "http://127.0.0.1:5500","http://192.168.100.17:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
