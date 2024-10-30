10a5234ac244c4d3ba466ea7365440136771665c
Juan Sebastian Velasquez <sebastianvelasquez2807@gmail.com>
2024-10-30 11:58:34 -0500

    generando documentacion del back

--- src/main/java/edu/eci/cvds/Tasks/service/AuthService.java
+++ src/main/java/edu/eci/cvds/Tasks/service/AuthService.java
@@ -25,6 +25,11 @@ public class AuthService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    /**
+     * Método para autenticar el usuario en el sistema y generar un token de sesión si la contraseña es correcta
+     * @param user Usuario que se registró que contiene las credenciales proporcionadas por el usuario
+     * @return Token si la autenticación es exitosa, o null si las el usuario no existe
+     */
     public Token logIn(User user) {
         Optional<User> optionalUser = userRepository.findByUserName(user.getUsername());
 
@@ -43,6 +48,11 @@ public class AuthService {
         }
     }
 
+    /**
+     * Método para crear un usuario asignandole como rol predeterminado el rol de usuario y asignandole la contraseña encriptada 
+     * @param user Usuario que se registró
+     * @return Usuario con la contraseña nueva y encriptada y la lista de roles como rol predeterminado el de usuario
+     */
     public User createUser(User user) {
         Optional<User> optionalUser = userRepository.findByUserName(user.getUsername());
         if (optionalUser.isEmpty()) {
@@ -54,10 +64,20 @@ public class AuthService {
         }
     }
 
+    /**
+     * Método para hacer el logout de la aplicación
+     * @param token Token del usuario que quiere salir de la aplicación para eliminarlo
+     */
     public void logOut(Token token) {
         tokenRepository.deleteById(token.getIdToken());
     }
 
+    /**
+     * Método para asignar un rol a un usuario ya existente
+     * @param userId ID del usuario que se le quiere añadir un rol
+     * @param role Rol que se le quiere agregar al usuario
+     * @return Usuario con el nuevo rol ya asignado
+     */
     public User assignRoleToUser(String userId, String role) {
         Optional<User> optionalUser = userRepository.findById(userId);
         if (optionalUser.isPresent()) {
@@ -70,6 +90,11 @@ public class AuthService {
         }
     }
 
+    /**
+     * Método para obtener los roles de un usuario mediante el nombre de usuario
+     * @param userName Nombre del usuario del que se quieren obtener los roles
+     * @return Lista de strings que contiene los roles del usuario
+     */
     public List<String> getUserRolesByUserName(String userName) {
         Optional<User> optionalUser = userRepository.findByUserName(userName);
         if (optionalUser.isPresent()) {
