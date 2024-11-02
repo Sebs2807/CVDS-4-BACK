5d3463dcfad0e7700f7a52862993a44b91462440
David Velasquez <david.velasquez-c@mail.escuelaing.edu.co>
2024-11-01 23:05:32 -0500

    documentacion back

--- src/main/java/edu/eci/cvds/Tasks/repository/TaskRepository.java
+++ src/main/java/edu/eci/cvds/Tasks/repository/TaskRepository.java
@@ -1,11 +1,21 @@
 package edu.eci.cvds.Tasks.repository;
 
 import edu.eci.cvds.Tasks.model.Task;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
+/**
+ * Repository interface for managing {@link Task} entities.
+ * Extends the {@link MongoRepository} interface to provide CRUD operations.
+ */
 public interface TaskRepository extends MongoRepository<Task, String> {
+
+    /**
+     * Finds all tasks associated with a specific user ID.
+     *
+     * @param idUser the ID of the user whose tasks are to be retrieved.
+     * @return a list of tasks associated with the specified user ID.
+     */
     List<Task> findByidUser(String idUser);
 }
--- src/main/java/edu/eci/cvds/Tasks/repository/UserRepository.java
+++ src/main/java/edu/eci/cvds/Tasks/repository/UserRepository.java
@@ -5,6 +5,19 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
+/**
+ * Interfaz de repositorio para gestionar entidades {@link User}.
+ * Extiende la interfaz {@link MongoRepository} para proporcionar operaciones
+ * CRUD.
+ */
 public interface UserRepository extends MongoRepository<User, String> {
+
+    /**
+     * Encuentra un usuario por su nombre de usuario.
+     *
+     * @param username el nombre de usuario del usuario a encontrar.
+     * @return un {@link Optional} que contiene el usuario si se encuentra, o vacío
+     *         si no se encuentra.
+     */
     Optional<User> findByUserName(String username);
 }
